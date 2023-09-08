9e5b925ed8cc1d41dcd17b403e51c7412bc6d2a9
garenboz <38819076+garenboz@users.noreply.github.com>
2023-09-08 13:15:09 +0300

    Update Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -544,8 +544,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Botomotiv Chatbot</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Botomotiv Chatbot</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
